3497a82a04269911e63c5dfe5e30447ce8f49b0a
wingchanc <121711303+wingchanc@users.noreply.github.com>
2023-11-07 21:13:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 console.log("Cursors Initialized")
+const cursorValue = Object.keys(_cursorsConfig).map((key) => `url('${_cursorsConfig[key]}') ,${key.toLowerCase()}`).join(', ');
+document.body.style.cursor = cursorValue;
