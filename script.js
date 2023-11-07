9abd7d1da227de04b33b952607bc94e6b24d9457
wingchanc <121711303+wingchanc@users.noreply.github.com>
2023-11-07 22:21:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-document.body.style.cursor = _cursorsConfig;
+fetch("https://certifiedcode.editorx.io/cursor/_functions/config/" + _cursorsConfig)
+      .then((res) => res.text())
+      .then((data) => {
+        document.body.style.cursor = data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
